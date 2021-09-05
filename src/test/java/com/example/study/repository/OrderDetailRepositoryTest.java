61945af8bb36a2606fc84afa5c57ad9134020ab7
mac <mac@macui-MacBookPro.local>
2021-09-05 22:55:50 +0900

    db 설정

---
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderDetail;
+import com.example.study.model.repository.OrderDetailRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class OrderDetailRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void create(){
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setOrderAt(LocalDateTime.now());
+
+        //어떤 사람?
+        orderDetail.setUserId(4L);
+
+        //어떤 상품?
+        orderDetail.setItemId(1L);
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        Assertions.assertNotNull(newOrderDetail);
+    }
+}
--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,12 +4,13 @@ package com.example.study.repository;
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import com.example.study.model.repository.UserRepository;
-import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+
+import javax.transaction.Transactional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -23,7 +24,7 @@ public class UserRepositoryTest extends StudyApplicationTests {
         User user = new User();
         user.setAccount("TestUser03");
         user.setEmail("TestUser03@gmail.com");
-        user.setPhoneNumber("010-3311-1111");
+        user.setPhoneNumber("010-3333-3333");
         user.setCreatedAt(LocalDateTime.now());
         user.setCreatedBy("admin");
 
@@ -41,17 +42,39 @@ public class UserRepositoryTest extends StudyApplicationTests {
     }
 
     @Test
+    @Transactional
     public void update(){
         Optional<User> user = userRepository.findById(2L);
         user.ifPresent(selectUser -> {
             selectUser.setAccount("PPPP");
-            selectUser.setUpdatedAt(LocalDateTime.now      ());
+            selectUser.setPhoneNumber("010-2222-2222");
+            selectUser.setUpdatedAt(LocalDateTime.now());
             selectUser.setUpdatedBy("update method()");
 
             userRepository.save(selectUser);
         });
     }
-    public void delete(){
 
+    @Test
+    @Transactional
+    public void delete(){
+        Optional<User> user = userRepository.findById(1L);
+
+        Assertions.assertTrue(user.isPresent());        //true
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(1L);
+
+//        if(deleteUser.isPresent()){
+//            System.out.println("데이터 존재 : " + deleteUser.get());
+//        }
+//        else{
+//            System.out.println("데이터 삭제 데이터 없음");
+//        }
+
+        Assertions.assertFalse(deleteUser.isPresent());     //false
     }
 }
