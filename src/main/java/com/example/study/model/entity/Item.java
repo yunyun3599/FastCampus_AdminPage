0df9267df2dad30a910a30c33c602d9aa22ceca6
yunyun3599 <yunyun3599@gmail.com>
2021-09-06 22:40:00 +0900

    entity간 관계 설정

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -5,10 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -25,4 +23,8 @@ public class Item {
     private Integer price;
 
     private String content;
+
+    //1:N
+    @OneToMany(fetch=FetchType.LAZY, mappedBy = "item")
+    private List<OrderDetail> orderDetailList;
 }
