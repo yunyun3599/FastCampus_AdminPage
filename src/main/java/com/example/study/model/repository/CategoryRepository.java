cc86dbb025d67faff90e106a2e84da53f9cc1598
Yoonjae <yunyun3599@gmail.com>
2021-09-07 21:44:32 +0900

    repository 생성

--- src/main/java/com/example/study/model/repository/CategoryRepository.java
+++ src/main/java/com/example/study/model/repository/CategoryRepository.java
@@ -1,12 +1,11 @@
 package com.example.study.model.repository;
 
-import com.example.study.model.entity.User;
+import com.example.study.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 
 }
