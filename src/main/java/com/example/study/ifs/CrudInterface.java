a2add40ca34dbede69d320cff6832f6c26171598
Yoonjae <yunyun3599@gmail.com>
2021-09-12 23:00:40 +0900

    response의 header 생성

---
+++ src/main/java/com/example/study/ifs/CrudInterface.java
@@ -0,0 +1,15 @@
+package com.example.study.ifs;
+
+import com.example.study.model.network.Header;
+
+public interface CrudInterface {
+
+    Header create();        //todo request 추가
+
+    Header read(Long id);
+
+    Header update();
+
+    Header delete(Long id);
+
+}
--- src/main/java/com/example/study/model/network/Header.java
+++ src/main/java/com/example/study/model/network/Header.java
@@ -1,22 +1,56 @@
 package com.example.study.model.network;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Header {
+public class Header<T> {
 
     //api 통신 시간
-    private String transactionTime;
+    @JsonProperty("transaction_time")
+    private LocalDateTime transactionTime;
 
     //api 응답 코드
     private String resultCode;
 
     //api 부가 설명명
     private String description;
+
+    private T data;
+
+    //OK
+    public static <T> Header<T> OK(){
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .build();
+    }
+
+    //Data OG
+    public static <T> Header<T> OK(T data){
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .data(data)
+                .build();
+    }
+
+    //ERROR
+    public static <T> Header<T> ERROR(String description){
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("ERROR")
+                .description(description)
+                .build();
+    }
 }
